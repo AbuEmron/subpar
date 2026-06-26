a368429cd0f1b74f5b9c20713d8dc8ffca2fe66b
AbuEmron <muhammadjt2023@gmail.com>
2026-06-26 15:37:17 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='subpar-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin){return;}
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
